d76a1c052a56570593bef5bdbb8a080400b5f17a
Ben F <CivisMiles@users.noreply.github.com>
2015-12-03 13:13:19 -0600

    Update 9GAG_DDT_Next_Version_Beta.user.js

--- 9GAG_DDT_Next_Version_Beta.user.js
+++ 9GAG_DDT_Next_Version_Beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         9GAG DDT *Next Version Changes*
 // @description  !!!!TEST Version!!!! A dark theme for 9GAG on desktop (non-mobile) environment !!!!TEST Version!!!!
-// @version      0.8.0.3 beta
+// @version      0.8.0.4 beta
 // @author       CivisMiles
 // @license      GNU GPL version 3 or any later version; https://github.com/CivisMiles/Dark-9GAG-Desktop-Theme/blob/master/LICENSE; http://www.gnu.org/copyleft/gpl.html
 // @homepage     https://github.com/CivisMiles/Dark-9GAG-Desktop-Theme
@@ -53,7 +53,7 @@ document.documentElement.appendChild(NotifyMenu);
   Part   ---   Description/What it effects   -----------------------------------------------   Current text within "NotifyMenu.innerHTML"   -------------------------------------------   Extra Notes
     1   ----   Notification menu background and border color   -----------------------------   .notification-menu { background-color: #101010; border: 1px solid #999}   --------------   N/A
     2   ----   Notification menu title text color   ----------------------------------------   .notification-menu .title h3{ color: #999;}   ------------------------------------------   N/A
-    3   ----   New notification menu multi-notification background and border color   ------   .notification-list li a.item { background-color: #101010;}   ---------------------------   N/A
+    3   ----   Notification menu multi-notification background and border color   ----------   .notification-list li a.item { background-color: #101010;}   ---------------------------   N/A
     4   ----   New notification menu multi-notification background and border color   ------   .notification-list li a.item.new { background-color: #101010; border-color: #999;}   ---   N/A
     5   ----   Notification menu Single/last-notification background and border color   ----   .notification-list li:last-child a.item { background-color: #101010}   -----------------   N/A
     6   ----   Notification menu avatar background  color   --------------------------------   .notification-list .avatar-container { background-color: #999;}   ----------------------   N/A
